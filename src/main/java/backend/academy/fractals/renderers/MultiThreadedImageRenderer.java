edc03eabc49ad3defc7f3cf1f0e8c8b08752bf4a
qwatro2 <kv22828@gmail.com>
2024-11-25 20:49:49 +0300

    fit: add multithreading implementation of ImageRenderer interface

---
+++ src/main/java/backend/academy/fractals/renderers/MultiThreadedImageRenderer.java
@@ -0,0 +1,208 @@
+package backend.academy.fractals.renderers;
+
+import backend.academy.fractals.entities.Color;
+import backend.academy.fractals.entities.Pixel;
+import backend.academy.fractals.entities.PixelImage;
+import backend.academy.fractals.entities.Point;
+import backend.academy.fractals.factories.GeneratorWithBounds;
+import backend.academy.fractals.factories.NonlinearTransformationGeneratorFactory;
+import backend.academy.fractals.factories.PointGeneratorFactory;
+import backend.academy.fractals.generators.Generator;
+import backend.academy.fractals.params.Params;
+import backend.academy.fractals.transformations.LinearTransformation;
+import backend.academy.fractals.transformations.Transformation;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class MultiThreadedImageRenderer implements ImageRenderer {
+    private static final int NUMBER_OF_SKIPPED_ITERATIONS = 20;
+
+    private final int numberOfSamples;
+    private final int numberOfTransformations;
+    private final int numberOfIterationsPerSample;
+    private final int width;
+    private final int height;
+
+    private final Random random;
+    private final Generator<LinearTransformation> linearTransformationGenerator;
+    private final Generator<Transformation> nonlinearTransformationGenerator;
+    private final Generator<Color> colorGenerator;
+    private final Generator<Point> pointGenerator;
+
+    private final double xMin;
+    private final double xMax;
+    private final double yMin;
+    private final double yMax;
+
+    private final double numberOfSymmetries;
+
+    private final int numberOfThreads;
+    private final Thread[] threads;
+
+    public MultiThreadedImageRenderer(
+        Params params,
+        Generator<LinearTransformation> linearTransformationGenerator,
+        Generator<Color> colorGenerator,
+        PointGeneratorFactory pointGeneratorFactory,
+        NonlinearTransformationGeneratorFactory nonlinearTransformationGeneratorFactory,
+        int numberOfThreads
+    ) {
+        this.numberOfThreads = numberOfThreads;
+        this.threads = new Thread[this.numberOfThreads];
+        this.numberOfSamples = params.numbersParams().numberOfSamples() / this.numberOfThreads;
+
+        this.numberOfTransformations = params.numbersParams().numberOfTransformations();
+        this.numberOfIterationsPerSample = params.numbersParams().numberOfIterationsPerSample();
+        this.width = params.sizeParams().width();
+        this.height = params.sizeParams().height();
+
+        this.random = new Random();
+        if (params.seed() != null) {
+            this.random.setSeed(params.seed());
+        }
+
+        this.linearTransformationGenerator = linearTransformationGenerator.setRandom(this.random);
+        this.colorGenerator = colorGenerator.setRandom(this.random);
+
+        this.nonlinearTransformationGenerator =
+            nonlinearTransformationGeneratorFactory.produce(
+                params.transformationsParams().nonlinearTransformations(),
+                params.transformationsParams().generationOrder()
+            ).setRandom(this.random);
+
+        GeneratorWithBounds productionResult = pointGeneratorFactory.produce(this.width, this.height);
+        this.pointGenerator = productionResult.generator().setRandom(this.random);
+        this.xMin = productionResult.xMin();
+        this.xMax = productionResult.xMax();
+        this.yMin = productionResult.yMin();
+        this.yMax = productionResult.yMax();
+
+        this.numberOfSymmetries = params.numbersParams().numberOfSymmetries();
+    }
+
+    @Override
+    public PixelImage render() {
+        PixelImage canvas = new PixelImage(width, height);
+
+        List<LinearTransformation> linearTransformations =
+            generateList(linearTransformationGenerator, numberOfTransformations);
+        List<Color> colors = generateList(colorGenerator, numberOfTransformations);
+
+        for (int i = 0; i < numberOfThreads; ++i) {
+            threads[i] = new Thread(new MultiThreadedImageRendererInner(canvas, linearTransformations, colors));
+            threads[i].start();
+        }
+
+        for (int i = 0; i < numberOfThreads; ++i) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+
+        return canvas;
+    }
+
+    private void processSampleIterations(
+        List<LinearTransformation> linearTransformations,
+        List<Color> colors,
+        PixelImage canvas
+    ) {
+        Point point = pointGenerator.next();
+
+        for (int iter = -NUMBER_OF_SKIPPED_ITERATIONS; iter < numberOfIterationsPerSample; ++iter) {
+            int randomIndex = random.nextInt(numberOfTransformations);
+            Transformation linearTransformation =
+                linearTransformations.get(randomIndex);
+            Transformation nonlinearTransformation =
+                nonlinearTransformationGenerator.next();
+            Color color = colors.get(randomIndex);
+
+            point =
+                processOneSampleIterations(iter, linearTransformation, nonlinearTransformation, point, canvas, color);
+        }
+    }
+
+    private Point processOneSampleIterations(
+        int iter, Transformation linearTransformation,
+        Transformation nonlinearTransformation, Point point,
+        PixelImage canvas, Color color
+    ) {
+        point = linearTransformation.andThen(nonlinearTransformation).apply(point);
+
+        if (iter >= 0) {
+            double theta2 = 0.0;
+            for (int s = 0; s < numberOfSymmetries; ++s) {
+                theta2 += 2 * Math.PI / numberOfSymmetries;
+                double xRot = point.x() * Math.cos(theta2) - point.y() * Math.sin(theta2);
+                double yRot = point.x() * Math.sin(theta2) + point.y() * Math.cos(theta2);
+                Point pointRot = new Point(xRot, yRot);
+                if (isCorrectPoint(pointRot)) {
+                    int col = width - (int) (((xMax - pointRot.x()) / (xMax - xMin)) * width);
+                    int row = height - (int) (((yMax - pointRot.y()) / (yMax - yMin)) * height);
+
+                    if (0 <= col && col < width && 0 <= row && row < height) {
+                        // mutex lock
+                        synchronized (canvas.get(row, col)) {
+                            processCorrectRowCol(canvas, row, col, color);
+                        }
+                        // mutex unlock
+                    }
+                }
+            }
+        }
+
+        return point;
+    }
+
+    private void processCorrectRowCol(PixelImage canvas, int row, int col, Color color) {
+        Pixel current = canvas.get(row, col);
+
+        if (current.hitCount() == 0) {
+            current.color(color);
+        } else {
+            Color mixedColor = new Color(
+                (current.color().r() + color.r()) / 2,
+                (current.color().g() + color.g()) / 2,
+                (current.color().b() + color.b()) / 2
+            );
+            current.color(mixedColor);
+        }
+
+        current.increment();
+    }
+
+    private boolean isCorrectPoint(Point point) {
+        return xMin <= point.x() && point.x() <= xMax
+            && yMin <= point.y() && point.y() <= yMax;
+    }
+
+    private <T> List<T> generateList(Generator<T> generator, int size) {
+        return IntStream.range(0, size).mapToObj(_ -> generator.next()).toList();
+    }
+
+    private class MultiThreadedImageRendererInner implements Runnable {
+        private final PixelImage canvas;
+        private final List<LinearTransformation> linearTransformations;
+        private final List<Color> colors;
+
+        private MultiThreadedImageRendererInner(
+            PixelImage canvas,
+            List<LinearTransformation> linearTransformations,
+            List<Color> colors
+        ) {
+            this.canvas = canvas;
+            this.linearTransformations = linearTransformations;
+            this.colors = colors;
+        }
+
+        @Override
+        public void run() {
+            for (int num = 0; num < numberOfSamples; ++num) {
+                processSampleIterations(linearTransformations, colors, canvas);
+            }
+        }
+    }
+}
