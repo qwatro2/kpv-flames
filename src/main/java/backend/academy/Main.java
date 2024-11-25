091ba4b4a849ca430bee67ba648584c71d272a34
qwatro2 <kv22828@gmail.com>
2024-11-25 23:16:14 +0300

    fit: version 0.1

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,13 @@
 package backend.academy;
 
+import backend.academy.fractals.app.App;
+import backend.academy.fractals.app.FractalsApp;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+        App app = new FractalsApp(System.out, System.err);
+        app.run(args);
     }
 }
