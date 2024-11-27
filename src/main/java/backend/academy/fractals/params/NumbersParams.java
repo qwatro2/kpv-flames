f6cdd8b46717e279480328375fdacfa4b41c0a65
qwatro2 <kv22828@gmail.com>
2024-11-27 19:24:48 +0300

    style: remove magic numbers

--- src/main/java/backend/academy/fractals/params/NumbersParams.java
+++ src/main/java/backend/academy/fractals/params/NumbersParams.java
@@ -6,8 +6,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class NumbersParams {
-    private int numberOfSamples = 1000;
-    private int numberOfTransformations = 10;
-    private int numberOfIterationsPerSample = 100000;
-    private int numberOfSymmetries = 1;
+    private static final int DEFAULT_N_SAMPLES = 1000;
+    private static final int DEFAULT_N_TRANSFORMATIONS = 10;
+    private static final int DEFAULT_N_ITERATIONS = 100000;
+    private static final int DEFAULT_N_SYMMETRIES = 1;
+
+    private int numberOfSamples = DEFAULT_N_SAMPLES;
+    private int numberOfTransformations = DEFAULT_N_TRANSFORMATIONS;
+    private int numberOfIterationsPerSample = DEFAULT_N_ITERATIONS;
+    private int numberOfSymmetries = DEFAULT_N_SYMMETRIES;
 }
