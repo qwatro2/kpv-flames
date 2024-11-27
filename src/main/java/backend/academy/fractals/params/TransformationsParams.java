e971f396492b04d5028d4d409c7ee8e0d774be02
qwatro2 <kv22828@gmail.com>
2024-11-27 19:26:27 +0300

    style: order imports

--- src/main/java/backend/academy/fractals/params/TransformationsParams.java
+++ src/main/java/backend/academy/fractals/params/TransformationsParams.java
@@ -1,10 +1,10 @@
 package backend.academy.fractals.params;
 
 import backend.academy.fractals.transformations.Transformation;
-import lombok.Getter;
-import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
