bf9775da0dff8216ca2bbc88b6b87ba3f5029383
qwatro2 <kv22828@gmail.com>
2024-11-27 19:25:26 +0300

    style: remove magic numbers

--- src/main/java/backend/academy/fractals/params/SizeParams.java
+++ src/main/java/backend/academy/fractals/params/SizeParams.java
@@ -6,6 +6,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SizeParams {
-    private int width = 900;
-    private int height = 900;
+    private static final int DEFAULT_WIDTH = 900;
+    private static final int DEFAULT_HEIGHT = 900;
+
+    private int width = DEFAULT_WIDTH;
+    private int height = DEFAULT_HEIGHT;
 }
